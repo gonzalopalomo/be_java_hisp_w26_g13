baf72fed9ba1a6c3505813a54ae2767c955689f5
gonzalopalomo <gonzalo.palomo@mercadolibre.com>
2024-04-23 14:24:53 -0300

    Armando esqueleto del proyecto

---
+++ be_java_hisp_w26_g13/src/main/java/com/be_java_hisp_w26_g13/be_java_hisp_w26_g13/exception/ExceptionController.java
@@ -0,0 +1,20 @@
+package com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.exception;
+
+import com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.dto.ExceptionDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.IOException;
+
+@ControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> notFound(NotFoundException e){
+        ExceptionDto exceptionDto = new ExceptionDto(e.getMessage());
+        return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
+    }
+
+
+}
