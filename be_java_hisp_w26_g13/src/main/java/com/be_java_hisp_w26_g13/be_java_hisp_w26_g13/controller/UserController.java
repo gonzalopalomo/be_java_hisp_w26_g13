8d962321021b3a178f1c9869c25914f4d5720062
miaramosml <mia.ramos@mercadolibre.com>
2024-04-23 17:36:08 -0300

    agrego el controller para el path del caso de uso

--- be_java_hisp_w26_g13/src/main/java/com/be_java_hisp_w26_g13/be_java_hisp_w26_g13/controller/UserController.java
+++ be_java_hisp_w26_g13/src/main/java/com/be_java_hisp_w26_g13/be_java_hisp_w26_g13/controller/UserController.java
@@ -2,14 +2,20 @@ package com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.controller;
 
 import com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
     @Autowired
     IUserService iUserService;
+
+    //metodo GET para el us-0003
+    @GetMapping("/{userId}/followers/list")
+    ResponseEntity<?>followersList(@PathVariable int userId){
+        return new ResponseEntity<>("" + userId, HttpStatus.OK);
+    }
 
 }
