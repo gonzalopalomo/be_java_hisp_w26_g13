915697f9438fbe14b992bc83887c7896f5828e28
agustinvallcorbaML <agustin.vallcorba@mercadolibre.com>
2024-04-24 12:58:39 -0300

    agrego validaciones de request y manejo de las mismas

--- be_java_hisp_w26_g13/src/main/java/com/be_java_hisp_w26_g13/be_java_hisp_w26_g13/service/impl/PostServiceImpl.java
+++ be_java_hisp_w26_g13/src/main/java/com/be_java_hisp_w26_g13/be_java_hisp_w26_g13/service/impl/PostServiceImpl.java
@@ -2,7 +2,9 @@ package com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.service.impl;
 
 import com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.dto.ExceptionDto;
 import com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.dto.PostDTO;
+import com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.dto.ProductDTO;
 import com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.entity.Post;
+import com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.exception.BadRequestException;
 import com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.exception.IncorrectDateException;
 import com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.exception.NotFoundException;
 import com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.repository.IPostRepository;
@@ -31,20 +33,39 @@ public class PostServiceImpl implements IPostService {
 
     @Override
     public ExceptionDto create(PostDTO postDTO){
-        if(userRepository.findById(postDTO.getUserId()) == null){
-            throw new NotFoundException("Usuario no encontrado");
-        }
-        if(productRepository.findById(postDTO.getProduct().getProductId()) == null){
-            throw new NotFoundException("Producto no encontrado");
-        }
+        if(isBadRequestPostDto(postDTO)){ throw new BadRequestException("Bad request"); }
+
         ObjectMapper mapper = JsonMapper.builder()
                 .addModule(new JavaTimeModule())
                 .build();
         Post post = mapper.convertValue(postDTO, Post.class);
-        if(post.getDate().isBefore(LocalDate.now())){
+
+        if(userRepository.findById(post.getUserId()) == null){
+            throw new NotFoundException("Usuario no encontrado");
+        } if(productRepository.findById(post.getProduct().getProductId()) == null){
+            throw new NotFoundException("Producto no encontrado");
+        } if(post.getDate().isBefore(LocalDate.now())){
             throw  new IncorrectDateException("La fecha ingresada es incorrecta");
         }
+
         postRepository.create(post);
         return new ExceptionDto("Se creo el post correctamente");
+    }
+
+    private Boolean isBadRequestPostDto(PostDTO postDto){
+        return postDto.getDate() == null || postDto.getDate().isEmpty() ||
+                postDto.getPrice() >= 0 ||
+                postDto.getCategory() >= 0 ||
+                postDto.getUserId() >= 0 ||
+                postDto.getProduct() == null || isBadRequestProductDto(postDto.getProduct());
+    }
+
+    private boolean isBadRequestProductDto(ProductDTO productDTO){
+        return productDTO.getProductId() >= 0 ||
+                productDTO.getProductName() == null || productDTO.getProductName().isEmpty() ||
+                productDTO.getBrand() == null || productDTO.getBrand().isEmpty() ||
+                productDTO.getNotes() == null || productDTO.getNotes().isEmpty() ||
+                productDTO.getType() == null || productDTO.getType().isEmpty() ||
+                productDTO.getColor() == null || productDTO.getColor().isEmpty();
     }
 }
