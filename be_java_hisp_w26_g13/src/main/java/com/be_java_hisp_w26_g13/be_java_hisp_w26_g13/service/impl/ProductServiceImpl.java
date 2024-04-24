76cfdaccff6a5e90143911d2fdebd58d58b659ce
erquispe <leandro.quispe@mercadolibre.com>
2024-04-24 16:48:35 -0300

    us-0009 agregar funcion ordenamiento por fecha

--- be_java_hisp_w26_g13/src/main/java/com/be_java_hisp_w26_g13/be_java_hisp_w26_g13/service/impl/ProductServiceImpl.java
+++ be_java_hisp_w26_g13/src/main/java/com/be_java_hisp_w26_g13/be_java_hisp_w26_g13/service/impl/ProductServiceImpl.java
@@ -1,8 +1,21 @@
 package com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.service.impl;
 
+import com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.entity.Post;
 import com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.service.IProductService;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
+import java.util.List;
+
 @Service
 public class ProductServiceImpl implements IProductService {
+
+    private List<Post> orderByDate(List<Post> posts, String order){
+        if(order.equals("date_asc")){
+            posts.sort(Comparator.comparing(Post::getDate));
+        } else if(order.equals("date_desc")){
+            posts.sort(Comparator.comparing(Post::getDate).reversed());
+        }
+        return posts;
+    }
 }
