45aad56b8606d7cd80ba24ded41e13e27f1da316
miaramosml <mia.ramos@mercadolibre.com>
2024-04-23 21:07:40 -0300

    agregue a userRepositoryImpl seguidos y seguidores a los usuarios creados

--- be_java_hisp_w26_g13/src/main/java/com/be_java_hisp_w26_g13/be_java_hisp_w26_g13/repository/impl/UserRepositoryImpl.java
+++ be_java_hisp_w26_g13/src/main/java/com/be_java_hisp_w26_g13/be_java_hisp_w26_g13/repository/impl/UserRepositoryImpl.java
@@ -17,21 +17,46 @@ public class UserRepositoryImpl implements IUserRepository {
     }
 
     private void initializeUsers() {
-        listUser.add(new User(1, "AliceMorrison"));
-        listUser.add(new User(2, "BobSmith"));
-        listUser.add(new User(3, "CharlieGarcia"));
-        listUser.add(new User(4, "DaisyJohnson"));
-        listUser.add(new User(5, "EdwardWilson"));
-        listUser.add(new User(6, "FionaCampbell"));
-        listUser.add(new User(7, "GeorgeMiller"));
-        listUser.add(new User(8, "HannahScott"));
-        listUser.add(new User(9, "IanPeterson"));
-        listUser.add(new User(10, "JuliaEvans"));
-        listUser.add(new User(11, "KevinBrown"));
-        listUser.add(new User(12, "LauraWhite"));
-        listUser.add(new User(13, "MikeDavis"));
-        listUser.add(new User(14, "NoraBaker"));
-        listUser.add(new User(15, "OscarLee"));
+        User aliceMorrison = new User(1, "AliceMorrison");
+        User bobSmith = new User(2, "BobSmith");
+        User charlyGarcia = new User(3, "CharlieGarcia");
+        User daisyJohnson = new User(4, "DaisyJohnson");
+        User edwardWilson = new User(5, "EdwardWilson");
+        User fionaCampbell = new User(6, "FionaCampbell");
+        User goergeMiller = new User(7, "GeorgeMiller");
+        User hannahScott = new User(8, "HannahScott");
+        User ianPeterson = new User(9, "IanPeterson");
+        User juliaEvans = new User(10, "JuliaEvans");
+        User kevinBrown = new User(11, "KevinBrown");
+        User lauraWhite = new User(12, "LauraWhite");
+        User mikeDavis = new User(13, "MikeDavis");
+        User noraBaker = new User(14, "NoraBaker");
+        User oscarLee = new User(15, "OscarLee");
+
+        aliceMorrison.setFollowed(List.of(bobSmith, charlyGarcia));
+        aliceMorrison.setFollowers(List.of(oscarLee));
+        bobSmith.setFollowed(List.of(charlyGarcia));
+        bobSmith.setFollowers(List.of(aliceMorrison, oscarLee));
+        oscarLee.setFollowed(List.of(bobSmith, aliceMorrison));
+        oscarLee.setFollowers(List.of(charlyGarcia));
+        charlyGarcia.setFollowed(List.of(oscarLee));
+        charlyGarcia.setFollowers(List.of(aliceMorrison, bobSmith));
+
+        listUser.add(aliceMorrison);
+        listUser.add(bobSmith);
+        listUser.add(charlyGarcia);
+        listUser.add(daisyJohnson);
+        listUser.add(edwardWilson);
+        listUser.add(fionaCampbell);
+        listUser.add(goergeMiller);
+        listUser.add(hannahScott);
+        listUser.add(ianPeterson);
+        listUser.add(juliaEvans);
+        listUser.add(kevinBrown);
+        listUser.add(lauraWhite);
+        listUser.add(mikeDavis);
+        listUser.add(noraBaker);
+        listUser.add(oscarLee);
     }
 
     @Override
