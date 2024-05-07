25632738f89f5e603066af92221c9a928da3d26d
gonzalopalomo <gonzalo.palomo@mercadolibre.com>
2024-05-07 11:25:03 -0300

    Se creo clase UserServiceTest

---
+++ be_java_hisp_w26_g13/src/test/java/com/be_java_hisp_w26_g13/be_java_hisp_w26_g13/service/UserServiceTest.java
@@ -0,0 +1,9 @@
+package com.be_java_hisp_w26_g13.be_java_hisp_w26_g13.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+
+public class UserServiceTest {
+}
